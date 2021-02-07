7e9b504506b055991a9b07885dc7b5754d2eee5d
Hajta2 <50477868+Hajta2@users.noreply.github.com>
2021-02-07 19:25:06 +0100

    First Commit

--- graph.hpp
+++ graph.hpp
@@ -1,6 +1,8 @@
 #ifndef PARALLELIZING_GRAPH_ALGORITHMS_GRAPH_HPP
 #define PARALLELIZING_GRAPH_ALGORITHMS_GRAPH_HPP
 
+#include <chrono>
+#include <functional>
 #include <iostream>
 #include <random>
 #include <vector>
@@ -10,55 +12,94 @@ class Graph{
 private:
     std::vector<int> weights;
     std::vector<int> neighbourMatrix;
+    const int NOVertices;
+    std::vector<int> weightedFlow;
+
+
+    void getWeightedFlow(){
+        std::vector<int> res(NOVertices);
+
+        for (int i = 0; i < NOVertices; ++i) {
+            int sum = 0;
+            for (int j = 0; j < NOVertices; ++j) {
+                sum += weights[i]*neighbourMatrix[i*NOVertices+j];
+            }
+            res[i] = sum;
+        }
+        weightedFlow = res;
+    }
+
 public:
-    Graph(int edges, int vertices){
+    Graph(int edges, int vertices) : NOVertices(vertices) {
         std::random_device rd;
         std::mt19937_64 gen(rd());
         std::uniform_int_distribution<int> dis(0,vertices-1);
         std::vector<int> tmp(vertices*vertices);
+        std::vector<int> tmp2(vertices);
 
         int from;
         int to;
         for (int i = 0; i < edges; ++i){
             from = dis(gen);
             to = dis(gen);
-            while(from==to){
+            while(from==to && tmp[from*vertices+to] == 1){
                 from = dis(gen);
                 to = dis(gen);
             }
             tmp[from*vertices+to] = 1;
         }
-        neighbourMatrix=tmp;
+
+        for (int i = 0; i < vertices; ++i) {
+            tmp2[i] = dis(gen);
+        }
+
+        neighbourMatrix = tmp;
+        weights = tmp2;
     }
 
-    explicit Graph(int vertices){
+    explicit Graph(int vertices) : NOVertices(vertices) {
         std::random_device rd;
         std::mt19937_64 gen(rd());
-        std::uniform_int_distribution<int> dis2(0,vertices*vertices);
         std::uniform_int_distribution<int> dis(0,vertices-1);
+        std::uniform_int_distribution<int> dis2(0,vertices*(vertices-1));
         int edges = dis2(gen);
 
         std::vector<int> tmp(vertices*vertices);
+        std::vector<int> tmp2(vertices);
 
         int from;
         int to;
         for (int i = 0; i < edges; ++i){
             from = dis(gen);
             to = dis(gen);
-            while(from==to){
+            while(from==to && tmp[from*vertices+to] == 1){
                 from = dis(gen);
                 to = dis(gen);
             }
             tmp[from*vertices+to] = 1;
         }
-        neighbourMatrix=tmp;
+
+        for (int i = 0; i < vertices; ++i) {
+            tmp2[i] = dis(gen);
+        }
+
+        neighbourMatrix = tmp;
+        weights = tmp2;
 
     }
 
+    int measure(){
+        auto start = std::chrono::high_resolution_clock::now();
+        getWeightedFlow();
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto duration = duration_cast<std::chrono::microseconds>(stop - start);
+        return duration.count();
+    }
+
     void print(){
-        for (int i = 0; i < 8; ++i) {
-            for (int j = 0; j < 8; ++j) {
-                std::cout<<neighbourMatrix[i*sqrt(neighbourMatrix.size())+j]<<" ";
+        for (int i = 0; i < NOVertices; ++i) {
+            for (int j = 0; j < NOVertices; ++j) {
+                std::cout<<neighbourMatrix[i*NOVertices+j]<<" ";
                 if(j==7){
                     std::cout<<std::endl;
                 }
--- main.cpp
+++ main.cpp
@@ -2,11 +2,9 @@
 #include <iostream>
 
 
+
 int main() {
-    Graph g(18,8);
-    g.print();
-    std::cout<<std::endl;
-    Graph g2(8);
-    g2.print();
+    Graph g2(512);
+    std::cout<< g2.measure();
     return 0;
 }

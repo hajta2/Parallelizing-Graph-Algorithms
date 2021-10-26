b691e0d264756261129ae759646998965484a298
Hajta2 <topitomi22@gmail.com>
2021-10-26 06:38:00 +0000

    arm implementation of spmv

--- include/graphCSR.hpp
+++ include/graphCSR.hpp
@@ -4,6 +4,7 @@
 #include "graphCOO.hpp"
 #include <omp.h>
 #include <cassert>
+#include "armpl.h"
 
 class GraphCSR : public AbstractGraph {
 private:
@@ -14,6 +15,18 @@ private:
     std::vector<float> flow;
     Type type;
     const int NOVertices;
+    armpl_spmat_t csrA;
+
+    void getWeightedFlowARM() {
+        armpl_status_t result = armpl_spmv_exec_s(ARMPL_SPARSE_OPERATION_NOTRANS,
+            1.0f,
+            csrA,
+            weights.data(),
+            0.0f,
+            flow.data()
+        );
+        assert(result == ARMPL_STATUS_SUCCESS)
+    }
 
     void naiveFlow() {
         std::vector<float> res(NOVertices);
@@ -63,7 +76,18 @@ public:
         //NONonZeroElements
         int NONonZeros = csrColInd.size();
         csrRowPtr.push_back(NONonZeros);
+        armpl_status_t armMatrix = armpl_spmat_create_csr_s(
+            &csrA,
+            NOVertices,
+            NOVertices,
+            csrRowPtr.data(),
+            csrColInd.data(),
+            csrVal.data()
+        );
+        assert( armMatrix == ARMPL_STATUS_SUCCESS );
     }
+
+    ~GraphCSR() { armpl_spmat_destroy(csrA); }
 
     double bandWidth() {
 
